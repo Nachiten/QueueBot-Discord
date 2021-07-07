5539e5c36daf0cb42cefb3e493bf48637f8a0942
Ignacio Baptista <39410888+Nachiten@users.noreply.github.com>
2021-07-07 03:03:48 +0000

    Added basic funcionallity

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hola")
+import os
+import discord
+
+client = discord.Client()
+token = os.environ['TOKEN']
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'
+  .format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello!')
+
+  if message.content.startswith('!miNombre'):
+    await message.channel.send(message)
+
+client.run(token)
