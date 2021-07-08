45984e95a11b3e4d5af5992067fe5c774c0b42f3
Ignacio Baptista <39410888+Nachiten@users.noreply.github.com>
2021-07-08 01:43:33 +0000

    Refactor otro.

--- comandoHelp.py
+++ comandoHelp.py
@@ -7,23 +7,21 @@ from globalVariables import GlobalVariables
 # Access: Everyone
 async def manejarComandoHelp(mensaje, autorMensaje, tagAlAutor):
     canalSpamComandos = GlobalVariables.canalSpamComandos
-    
+
     # Creacion de mensaje embed
     mensajeEmbed = discord.Embed(title="Lista de comandos:",
                                  color=discord.Color.purple())
     mensajeEmbed.set_thumbnail(url="https://i.imgur.com/FU1z6dq.png")
-    mensajeEmbed.add_field(
-        name="Comandos para todos:",
-        value=
-        "!queue add unaCola | Agregarse a una cola\n!queue remove unaCola | Quitarse de una cola",
-        inline=False)
-    mensajeEmbed.add_field(
-        name="Comandos para Ayudantes:",
-        value="!queue create unaCola | Crear una nueva cola\n"
-        "!queue delete unaCola | Eliminar una cola\n"
-        "!queue next unaCola | Atender el siguiente en una cola\n"
-        "!queue list unaCola | Mostrar estado de la cola\n"
-        "!queue all | Mostrar todas las colas existentes",
+    mensajeEmbed.add_field(name="Comandos para todos:",
+                           value="!queue add unaCola | Agregarse a una cola\n"
+                           "!queue remove unaCola | Quitarse de una cola",
+                           inline=False)
+    mensajeEmbed.add_field(name="Comandos para Ayudantes:",
+                           value="!queue create unaCola | Crear una nueva cola\n"
+                           "!queue delete unaCola | Eliminar una cola\n"
+                           "!queue next unaCola | Atender el siguiente en una cola\n"
+                           "!queue list unaCola | Mostrar estado de la cola\n"
+                           "!queue all | Mostrar todas las colas existentes",
         inline=False)
     mensajeEmbed.add_field(
         name="Emojis:",
